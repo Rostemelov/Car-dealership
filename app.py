9ce895612a5aaea11c247e906c2b8dd0870044df
Sai Harshit.B <89305971+Rostemelov@users.noreply.github.com>
2023-01-12 19:08:00 +0530

    updated database and set the foreign keys

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import update
 from datetime import datetime
-
 
 
 
@@ -26,6 +26,8 @@ class Car(db.Model):
     color = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
+    sales = db.relationship('Sales', backref='car', lazy=True)
+
     def __repr__(self) -> str:
         return '<Car %r>' % self.car_id
 
@@ -34,13 +36,15 @@ class Customer(db.Model):
     name = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(80), nullable=False)
     phone = db.Column(db.Integer, nullable=False)
+    sales = db.relationship('Sales', backref='customer', lazy=True)
     def __repr__(self) -> str:
         return '<Customer %r>' % self.customer_id
 
 class Showroom(db.Model):
     showroom_id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(80), nullable=False)
-    manager_id = db.Column(db.Integer, nullable=False)
+    manager_id = db.Column(db.Integer, db.ForeignKey('manager.manager_id'), nullable=False)
+    sales = db.relationship('Sales', backref='showroom', lazy=True)
     def __repr__(self) -> str: 
         return '<Showroom %r>' % self.showroom_id
 
@@ -48,17 +52,18 @@ class Manager(db.Model):
     manager_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     password=db.Column(db.String(80), nullable=False)
+    showroom = db.relationship('Showroom', backref='manager', lazy=True)
     def __repr__(self) -> str: 
         return '<Manager %r>' % self.manager_id
 
 
 class Sales(db.Model):
     sales_id = db.Column(db.Integer, primary_key=True)
-    car_id = db.Column(db.Integer, nullable=False)
-    customer_id = db.Column(db.Integer, nullable=False)
-    showroom_id = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.DateTime(80), default=datetime.utcnow, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('car.car_id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), nullable=False)
+    showroom_id = db.Column(db.Integer, db.ForeignKey('showroom.showroom_id'), nullable=False)
+    date = db.Column(db.DateTime(80), default=datetime.utcnow)
+    amount = db.Column(db.Integer, nullable=False)
     def __repr__(self) -> str:
         return '<Sales %r>' % self.sales_id
 
@@ -71,28 +76,59 @@ with app.app_context():
 
 
 #Define structure of the app
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    #Add operation
+    if(request.method=='POST'):
+        car_id=request.form['car_id']
+        model=request.form['model']
+        year=request.form['year']
+        color=request.form['color']
+        price=request.form['price']
+        quantity=request.form['quantity']
+        new_car=Car(car_id=car_id, model=model, year=year, color=color, price=price, quantity=quantity)
+        db.session.add(new_car)
+        db.session.commit()
+        return redirect('/')
+    carsdb=Car.query.all()
+    return render_template('login.html', carsdb=carsdb)
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    #return render_template('add_removeCar.html')
+    #Update operation
+    if(request.method=='POST'):
+        carid=request.form['car_id']
+        quanTity=request.form['quantity']
+        db.session.query(Car).filter_by(car_id=carid).update({'quantity':Car.quantity+quanTity})
+        db.session.commit()
+        return redirect('/')
     return "Home Page"
 
-@app.route('/sell')
+@app.route('/sell', methods=['GET', 'POST'])
 def sell():
-   # return render_template('sell.html')
+   # sell operation
+   if(request.method=='POST'):
+        carid=request.form['car_id']
+        db.session.query(Car).filter_by(car_id=carid).update({'quantity':Car.quantity-1})
+        db.session.commit()
+        return redirect('/')
    return "Sell Page"
 
-@app.route('/view_db')
+@app.route('/view_db', methods=['GET', 'POST'])
 def viewdb():
-    #return render_template('viewdb.html')
+    #Delete operation
+    if(request.method=='POST'):
+        carid=request.form['car_id']
+        car_to_delete=Car.query.get(carid)
+        db.session.delete(car_to_delete)
+        db.session.commit()
+        return redirect('/')
     return "View DB Page"
 
 @app.route('/crud_db')
-def viewdb():
+def crud():
     #return render_template('viewdb.html')
+
     return "Setup DB Page"
 
 
